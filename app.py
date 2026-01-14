5093241a4d5c44c8f4006d29cea3059f1a50f70a
alanlopezromero <alopezromero192@gmail.com>
2026-01-13 18:26:38 -0600

    cambio en de models cliente

--- app.py
+++ app.py
@@ -90,19 +90,25 @@ def revisar_mensualidades():
             )
 
 class Cliente(db.Model):
-    __tablename__ = 'clientes'
+    __tablename__ = "clientes"
 
     id = db.Column(db.Integer, primary_key=True)
+
     nombre = db.Column(db.String(100), nullable=False)
     apellido = db.Column(db.String(100), nullable=False)
+
     email = db.Column(db.String(120), unique=True, nullable=True)
     password_hash = db.Column(db.String(256), nullable=True)
+
+    # 🔑 AQUÍ SE GUARDA EL QR (NO SE BORRA)
+    qr_base64 = db.Column(db.Text, nullable=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
 
 
 class Bebida(db.Model):
