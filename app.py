286ca1d63e420f7f3dd28cdba0641b7c5408bd67
alanlopezromero <alopezromero192@gmail.com>
2026-02-09 23:42:21 -0600

    cambio  de bloque de dbv10

--- app.py
+++ app.py
@@ -13,14 +13,13 @@ app.secret_key = os.environ.get("SECRET_KEY", "clave-temporal-dev")
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
-
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "poolclass": NullPool,
+    "pool_pre_ping": True,      # 🔥 ESTA ES LA QUE FALTABA
     "connect_args": {
         "sslmode": "require"
     }
 }
-
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
